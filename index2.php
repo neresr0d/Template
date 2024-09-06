6b076c700007b281b36d6d00fda955d6a01b1f33
neresr0d <rodrigoneres030@gmail.com>
2024-09-06 16:33:39 -0300

    template

--- index2.php
+++ index2.php
@@ -5,20 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rodrigo Neres</title>
-    <link rel="stylesheet" href="/template/template/CSS/style2.css">
+    <link rel="stylesheet" href="/template/CSS/style2.css">
 </head>
 
 <body>
     <header>
         <div class="nome-topo">
-            <h1>Rodrigo Neres._</h1>
+            <h1>Rodrigo Neres.<span>_</span></h1>
         </div>
         <div class="navegacao">
             <nav>
-                <a href="">Início</a>
                 <a href="">Habilidades</a>
                 <a href="">Projetos</a>
                 <a href="">Experiências</a>
+                <a href="">Cursos</a>
                 <a href="">Contatos</a>
             </nav>
 
@@ -31,8 +31,12 @@
     </main>
 
     <footer>
-    <div id="copy"><p>&copy; Rodrigo Neres</p></div>
-    <div><p>Todos os direitos reservados</p></div>
+    
+    <div>
+        <img src="/template/imgs/icon-whatsapp.svg" alt="" width="30px" height="30px">
+        <img src="/template/imgs/icon-github.svg" alt="" width="30px" height="30px">
+        <img src="/template/imgs/icon-instagram.svg" alt="" width="30px" height="30px">
+    </div>
     </footer>
 
 </body>
