067568f926cb78fdd2c4a6cbb4d8b1056ac04bde
neresr0d <rodrigoneres030@gmail.com>
2024-09-05 21:38:40 -0300

    iniciado outro template

--- index.php
+++ index.php
@@ -5,107 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rodrigo Neres</title>
+    <link rel="stylesheet" href="/template/template/CSS/style.css">
+
 </head>
 
-<style>
-    body,
-    html {
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        font-family: "Special Elite", system-ui;
-        min-width: 100vh;
-
-
-
-    }
-
-    html {
-        height: 100%;
-    }
-
-    .main {
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-position: center center;
-        flex-grow: 1;
-        left: 4rem;
-    }
-
-    #corpo {
-        background-image: url('/template/imgs/o_código_da_vinci.jpg');
-        background-size: cover;
-        background-position: center center;
-        background-repeat: no-repeat;
-        height: 100vh;
-
-    }
-
-
-
-    h1 {
-        color: black;
-    }
-
-    img {
-        width: 60px;
-        height: 60px;
-    }
-
-    .cont-superior {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 20px;
-        margin: 10px;
-
-    }
-
-    .cont-superior h1 {}
-
-    p {
-        text-align: center;
-        padding: 10px;
-
-    }
-
-    h2 {
-        text-align: center;
-    }
-
-    .aa {
-
-        justify-content: center;
-        flex-direction: column;
-        display: flex;
-        background-color: green;
-        border-radius: 20px;
-        width: 60px;
-        height: 50px;
-        padding: 8px;
-        margin: auto;
-
-    }
-
-    .aa a {
-        text-align: center;
-    }
-</style>
-
 <body>
+    <header>
+        <nav>
+
+            <a href="">Sobre mim</a>
+
+
+        </nav>
+
+    </header>
     <main class="template">
         <div id="corpo">
             <div class="cont-superior">
 
-                <nav>
-                    Sobre mim
-                </nav>
                 <div>
                     <h1>Rodrigo Pereira Neres</h1>
                 </div>
 
                 <div>
-                    <img src="/template/imgs/o_código_da_vinci.jpg" alt="">
+                    <img src="/template/template/imgs/o_código_da_vinci.jpg" alt="">
                 </div>
 
             </div>
---
+++ index2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rodrigo Neres</title>
+    <link rel="stylesheet" href="/template/template/CSS/style2.css">
+</head>
+
+<body>
+    <header>
+        <div class="nome-topo">
+            <h1>Rodrigo Neres._</h1>
+        </div>
+        <div class="navegacao">
+            <nav>
+                <a href="">Início</a>
+                <a href="">Habilidades</a>
+                <a href="">Projetos</a>
+                <a href="">Experiências</a>
+                <a href="">Contatos</a>
+            </nav>
+
+        </div>
+    </header>
+
+    <main>
+        <h1>Rodrigo Neres</h1>
+        <h2>Programador web iniciante</h2>
+    </main>
+
+    <footer>
+    <div id="copy"><p>&copy; Rodrigo Neres</p></div>
+    <div><p>Todos os direitos reservados</p></div>
+    </footer>
+
+</body>
+
+</html>
