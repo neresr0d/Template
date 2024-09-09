6610bce114195f9ad8d1e3000a91d7375d5f7a28
neresr0d <rodrigoneres030@gmail.com>
2024-09-09 19:46:57 -0300

    clone do repositório estante_web

--- _rodape.php
+++ _rodape.php
@@ -2,9 +2,9 @@
 
     <div>
         
-        <a href="https://github.com/neresr0d" target="_blank" rel="noopener noreferrer"><img src="/template/imgs/icon-github.svg" alt="" width="30px" height="30px" href="https://github.com/neresr0d"></a>
+        <a href="https://github.com/neresr0d" target="_blank" rel="noopener noreferrer"><img src="/template/imgs/github.svg" alt="" width="30px" height="30px" href="https://github.com/neresr0d"></a>
         
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><img src="/template/imgs/icon-instagram.svg" alt="" width="30px" height="30px" href="https://www.instagram.com"></a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><img src="/template/imgs/instagram.svg" alt="" width="30px" height="30px" href="https://www.instagram.com"></a>
         
     </div>
     
